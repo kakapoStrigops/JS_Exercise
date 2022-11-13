2c70cbfff998bb9f4af8b98266bf720f7b24196c
kakapoStrigops <emi.yamada1109@gmail.com>
2022-11-13 20:14:01 +0900

    [finish]1-4

--- example/index.js
+++ example/index.js
@@ -1,1 +1,12 @@
-1;
+// console.log(1 + 1);
+
+// const total = 42 + 42;
+// console.log(total);
+
+// console.log(1);
+
+// cosnt a=1;
+
+const value = "値";
+// console.log(x);
+console.log(value);
