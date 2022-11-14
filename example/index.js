c36d5c9d2528029496d35679c4db5545e9204b64
kakapoStrigops <emi.yamada1109@gmail.com>
2022-11-14 22:50:52 +0900

    [start]1-5 データ型とリテラル

--- example/index.js
+++ example/index.js
@@ -7,6 +7,17 @@
 
 // cosnt a=1;
 
-const value = "値";
-// console.log(x);
-console.log(value);
+// const value = "値";
+// // console.log(x);
+// console.log(value);
+
+console.log(typeof true);
+console.log(typeof 42);
+console.log(typeof 9007199254740992n);
+console.log(typeof "JavaScript");
+console.log(typeof Symbol("シンボル"));
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof ["配列"]);
+console.log(typeof {"key": "value"});
+console.log(typeof function(){});
